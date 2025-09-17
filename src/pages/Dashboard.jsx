fcb50ca5ca759ddfd52843cda0405f48482d64f0
Robert <robert.gebauer@koeln.de>
2025-09-17 13:12:55 +0200

    Schatten in der Tagesübersicht hinzugefügt

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -74,7 +74,7 @@ const Dashboard = () => {
     <div className="p-1">
       <div className="grid grid-cols-12 gap-2">
         {/* Linke Spalte – Tagesübersicht (oben), darunter MeineDienste */}
-        <div className="col-span-12 md:col-span-4 flex flex-col gap-2">
+        <div className="col-span-12 md:col-span-4 flex shadow-xl flex-col gap-2">
           {darfTagesuebersichtSehen && <TagesUebersicht />}
 
           {darfMeineDiensteSehen && <MeineDienste />}
